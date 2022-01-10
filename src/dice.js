1b9599d32d0173e2c511153ac296c32e6579c922
DarrenLe20 <89809192+DarrenLe20@users.noreply.github.com>
2022-01-10 16:51:18 -0500

    Add dice roll logic

--- src/dice.js
+++ src/dice.js
@@ -6,23 +6,26 @@ class Dice {
   roll() {
     let res = 0;
     switch (this.side) {
-      case 20:
+      case "20":
         res = Math.floor(Math.random() * 20) + 1;
         return res;
-      case 12:
+      case "12":
         res = Math.floor(Math.random() * 12) + 1;
         return res;
-      case 10:
+      case "10":
         res = Math.floor(Math.random() * 10);
         return res;
-      case 8:
+      case "8":
         res = Math.floor(Math.random() * 8) + 1;
         return res;
-      case 6:
+      case "6":
         res = Math.floor(Math.random() * 6) + 1;
         return res;
-      case 4:
+      case "4":
         res = Math.floor(Math.random() * 4) + 1;
+        return res;
+      default:
+        console.log("Default");
         break;
     }
   }
--- src/firstLoad.js
+++ src/firstLoad.js
@@ -1,8 +1,9 @@
 import Dice from "./dice";
 
 function addContent() {
-  let diceFlag = 0;
-  let numFlag = 0;
+  let diceFlag = 20;
+  let numFlag = 1;
+  let allDice = [];
   const page = document.querySelector("#content");
   // Header
   const header = document.createElement("div");
@@ -21,6 +22,33 @@ function addContent() {
   menu.classList.add("menu");
   const mainArea = document.createElement("div");
   mainArea.classList.add("mainArea");
+  // Build dice area
+  const rollBtn = document.createElement("button");
+  rollBtn.textContent = "ROLL";
+  const resetBtn = document.createElement("button");
+  resetBtn.textContent = "RESET";
+  const btnArea = document.createElement("div");
+  btnArea.classList.add("btnArea");
+  const diceArea = document.createElement("div");
+  diceArea.classList.add("diceArea");
+  const infoArea = document.createElement("p");
+  infoArea.classList.add("info");
+  infoArea.textContent =
+    "You are rolling (" + numFlag + ") D" + diceFlag + " dice!";
+  // Default dice
+  const defaultDice = document.createElement("div");
+  defaultDice.classList.add("20");
+  defaultDice.classList.add("dice");
+  defaultDice.textContent = 20;
+  diceArea.appendChild(defaultDice);
+  allDice.push(new Dice("20"));
+  // Append components
+  mainArea.appendChild(infoArea);
+  mainArea.appendChild(diceArea);
+  mainArea.appendChild(btnArea);
+  btnArea.appendChild(rollBtn);
+  btnArea.appendChild(resetBtn);
+
   // Build Menu: Choose dice type and choose number of dice
   const numRow = document.createElement("div");
   numRow.classList.add("numRow");
@@ -35,21 +63,27 @@ function addContent() {
   typeRow.classList.add("typeRow");
   const d20 = document.createElement("button");
   d20.classList.add("20");
+  d20.classList.add("dice");
   d20.textContent = "D20";
   const d12 = document.createElement("button");
   d12.classList.add("12");
+  d12.classList.add("dice");
   d12.textContent = "D12";
   const d10 = document.createElement("button");
   d10.classList.add("10");
+  d10.classList.add("dice");
   d10.textContent = "D10";
   const d8 = document.createElement("button");
   d8.classList.add("8");
+  d8.classList.add("dice");
   d8.textContent = "D8";
   const d6 = document.createElement("button");
   d6.classList.add("6");
+  d6.classList.add("dice");
   d6.textContent = "D6";
   const d4 = document.createElement("button");
   d4.classList.add("4");
+  d4.classList.add("dice");
   d4.textContent = "D4";
   typeRow.appendChild(d20);
   typeRow.appendChild(d12);
@@ -61,8 +95,10 @@ function addContent() {
   const allTypeBtn = typeRow.querySelectorAll("button");
   allTypeBtn.forEach((button) =>
     button.addEventListener("click", () => {
-      diceFlag = button.className;
-      console.log(diceFlag);
+      diceFlag = button.classList[0];
+      console.log("diceFlag = " + diceFlag);
+      // Create dice
+      createDice();
     })
   );
   // Create number flag
@@ -70,9 +106,64 @@ function addContent() {
   allNumBtn.forEach((button) =>
     button.addEventListener("click", () => {
       numFlag = button.className;
-      console.log(numFlag);
+      console.log("numFlag = " + numFlag);
+      // Create dice
+      createDice();
     })
   );
+
+  // Create dice based on flags
+  function createDice() {
+    while (diceArea.firstChild) {
+      diceArea.removeChild(diceArea.firstChild);
+    }
+    allDice = [];
+    if (numFlag != 0 && diceFlag != 0) {
+      for (let i = 1; i <= numFlag; i++) {
+        const diceObj = new Dice(diceFlag.toString());
+        allDice.push(diceObj);
+        const dice = document.createElement("div");
+        dice.classList.add(diceFlag);
+        dice.classList.add("dice");
+        dice.textContent = diceObj.getSide();
+        diceArea.appendChild(dice);
+      }
+    }
+    infoArea.textContent =
+      "You are rolling (" + numFlag + ") D" + diceFlag + " dice!";
+  }
+
+  // Roll dice
+  rollBtn.addEventListener("click", () => {
+    const all = diceArea.querySelectorAll(".dice");
+    let i = 0;
+    all.forEach(function (dice) {
+      console.log(allDice[i].getSide());
+      const res = allDice[i].roll();
+      console.log(res);
+      dice.textContent = res;
+      i++;
+    });
+  });
+
+  // Reset button
+  resetBtn.addEventListener("click", () => {
+    while (diceArea.firstChild) {
+      diceArea.removeChild(diceArea.firstChild);
+    }
+    let diceFlag = 20;
+    let numFlag = 1;
+    let allDice = [];
+    // Default dice
+    const defaultDice = document.createElement("div");
+    defaultDice.classList.add("20");
+    defaultDice.classList.add("dice");
+    defaultDice.textContent = 20;
+    diceArea.appendChild(defaultDice);
+    allDice.push(new Dice(20));
+    infoArea.textContent =
+      "You are rolling (" + numFlag + ") D" + diceFlag + " dice!";
+  });
   // Footer
   const footer = document.createElement("div");
   footer.classList.add("footer");
